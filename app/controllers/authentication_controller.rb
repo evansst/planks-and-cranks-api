987c7b54027bcdeaac9683935b80dc08cbe44b9f
Sam Evans <evansst@icloud.com>
2020-08-19 15:36:08 -0600

    adds login method to auth controller

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,6 +1,12 @@
 class AuthenticationController < ApplicationController
 
   def login
-    
+    @user = User.find_by(username: params[:username])
+
+    if !@user || !user.authenticate(params[:password])
+      render json: { message: 'Something is wrong with your Username or Password' }, status: :unauthorized
+    else
+      render json: { user: @user }
+    end
   end
 end
