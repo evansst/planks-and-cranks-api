532b64eb8e73e0964b4b56cdda64bae057b17b3a
Sam Evans <evansst@icloud.com>
2020-08-19 19:53:38 -0600

    adds token to login controller

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -3,10 +3,15 @@ class AuthenticationController < ApplicationController
   def login
     @user = User.find_by(username: params[:username])
 
-    if !@user || !user.authenticate(params[:password])
+    if !@user || !@user.authenticate(params[:password])
       render json: { message: 'Something is wrong with your Username or Password' }, status: :unauthorized
     else
-      render json: { user: @user }
+      token = JWT.encode(
+        { user_id: @user.id },
+        'some special secret string'
+      )
+
+      render json: { user: @user, token: token }, status: :ok
     end
   end
 end
